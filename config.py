1ef37efd2198613706b66231b1a688d18f10597d
Your GitHub Username <your.github.email@example.com>
2025-06-11 15:40:26 +0530

    Initial commit

--- config.py
+++ config.py
@@ -1,31 +1,31 @@
 # Main configuration file
 
 # Mandatory Variables
-API_ID = 1111111 # Replace with your actual Telegram API ID
-API_HASH = "your_api_hash_here"  # Replace with your actual Telegram API Hash
-BOT_TOKEN = "your_bot_token_here"  # Replace with your actual Bot Token
-OWNER_ID = "11111111"  # Replace with your actual Owner ID
+API_ID = 9330561 # Replace with your actual Telegram API ID
+API_HASH = "ec6fba589297d16c8c25201908e82412"  # Replace with your actual Telegram API Hash
+BOT_TOKEN = "7986169001:AAGTaiQX3oQj3lDCtAWijmYygbupu-hp2k0"  # Replace with your actual Bot Token
+OWNER_ID = "8111395095"  # Replace with your actual Owner ID
 # Database
-DATABASE_URL = "your_database_url_here"  # Replace with your actual database URL
+DATABASE_URL = "mongodb+srv://mdtahseen7378:IamTahseen730@wallp1.f6rtttp.mongodb.net/?retryWrites=true&w=majority&appName=wallp1"  # Replace with your actual database URL
 
-AUTH_CHAT = "-100123456789 -1001234567890" # Replace with your actual auth chat ID. You can use multiple IDs separated by ( space ).
-LOGS_CHAT = -1001234567891 # Replace with your actual logs chat ID
-POST_CHAT = -1001234567891 # Replace with your actual post chat ID
+AUTH_CHAT = "-1002829458686" # Replace with your actual auth chat ID. You can use multiple IDs separated by ( space ).
+LOGS_CHAT = -1002780565967 # Replace with your actual logs chat ID
+POST_CHAT = -1002780078654 # Replace with your actual post chat ID
 
-ADMIN_USERNAME = "admin" # Replace with your admin username
-ADMIN_PASSWORD = "adminadmin" # Replace with your admin password
+ADMIN_USERNAME = "EntStella" # Replace with your admin username
+ADMIN_PASSWORD = "Stella07" # Replace with your admin password
 
 
-SITE_SECRET = "your_secret_key" # Replace with your site secret key
-TMDB_API_KEY = "" # Replace with your TMDB API key
+SITE_SECRET = "a9X7cB2LmQ8zF1dE" # Replace with your site secret key
+TMDB_API_KEY = "a82ab5641c5430d0873025823ca9971b" # Replace with your TMDB API key
 
 # Optional Variables
 
 # If you want to use multiple bot tokens, uncomment the MULTI_TOKENS dictionary and add your tokens. this aditional bots will speed up the process of downloading and streaming files.
 MULTI_TOKENS = {
-    # 1: "BOT_TOKEN_1_HERE",
-    # 2: "BOT_TOKEN_2_HERE",
-    # Add more tokens as needed
+    1: "7882904808:AAEvov1XiBFsg69sXOAwVafNXiGEi_77ukE",
+    2: "7986169001:AAGTaiQX3oQj3lDCtAWijmYygbupu-hp2k0",
+    3: "7648223117:AAGSe3JZ7HXbpTK61YyLBK53cZRAYtAOBjU",
 }
 DELETE_AFTER_MINUTES = 10 # Set the number of minutes after which files will be deleted from user message
 POST_UPDATES = True # Set to True if you want to post updates in the post chat
